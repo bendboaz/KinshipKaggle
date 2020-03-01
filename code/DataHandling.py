65dec8c21b5596f426f13eb9b6784ef1af809f95
Boaz <bendboaz@gmail.com>
2020-03-01 11:18:04 +0200

    Initial commit

---
+++ code/DataHandling.py
@@ -0,0 +1,6 @@
+from torch.utils.data import 
+
+
+# Implement image reading function
+# Implement datasets (train, test, add validation split)
+# Later: augmentations (horizontal flips, color tweaks etc.)
---
+++ code/Models.py
@@ -0,0 +1,5 @@
+# Add imports
+
+# Load feature extractors (VGGFace, FaceNet, ResNet on Imagenet)
+
+# A (generic?) tool to take feature extractors, a combination function and create an end-to-end model.
