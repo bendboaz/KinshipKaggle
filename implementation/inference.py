3228abeede8fb847064f69992510a290c5079b5d
Boaz <bendboaz@gmail.com>
2020-04-18 21:50:52 +0300

    Added option for single model testing.

--- implementation/inference.py
+++ implementation/inference.py
@@ -1,6 +1,7 @@
 import os
 from argparse import ArgumentParser
 from functools import partial
+from typing import Optional, List, Iterable
 
 import torch
 from torch.utils.data import DataLoader
@@ -16,7 +17,8 @@ def test_collate_fn(samples):
     return torch.stack(list(image_pairs), dim=0), list(names)
 
 
-def infer(data_path, models_list, decision_type, threshold, top_k, batch_size, device, num_workers):
+def infer(data_path: str, models_list: Iterable[str], decision_type: NetworkEnsemble.DecisionMechanism,
+          threshold: Optional[float], top_k: Optional[int], batch_size=50, device=None, num_workers=4):
     device = torch.device('cpu' if device is None else device)
 
     raw_test_path = os.path.join(data_path, 'raw', 'test')
@@ -54,6 +56,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('data_path', type=str)
     parser.add_argument('experiments_path', type=str)
+    parser.add_argument('--single_model', type=str, default=None)
     parser.add_argument('--decision_type', type=str, nargs='*', choices=['AVG_POOLING', 'VOTING'])
     parser.add_argument('--threshold', type=float, default=None)
     parser.add_argument('--top_k', type=int, default=None)
@@ -62,8 +65,8 @@ if __name__ == '__main__':
     parser.add_argument('--save_dir', type=str)
 
     args = parser.parse_args()
-
-    full_paths = list(map(lambda exp: os.path.join(args.experiments_path, exp), ALL_MODELS))
+    relevant_models = ALL_MODELS if args.single_model is None else [args.single_model]
+    full_paths = list(map(lambda exp: os.path.join(args.experiments_path, exp), relevant_models))
     data_path = args.data_path
     if data_path is None:
         data_path = os.path.dirname(args.experiments_path)
